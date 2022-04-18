398da3cb8a879090f746854c0e307583b3ced045
Yufeng Liu <crazylyf@163.com>
2022-04-18 21:47:16 +0800

    add support for argmin for minimal pariwise distance estimation

--- math_utils.py
+++ math_utils.py
@@ -52,7 +52,7 @@ def calc_included_angles_from_coords(anchor_coords, coords1, coords2, return_rad
                 return_cos=return_cos)
     return angs
 
-def memory_safe_min_distances(voxels1, voxels2, num_thresh=50000):
+def memory_safe_min_distances(voxels1, voxels2, num_thresh=50000, return_index=False):
     # verified
     nv1 = len(voxels1)
     nv2 = len(voxels2)
@@ -63,6 +63,9 @@ def memory_safe_min_distances(voxels1, voxels2, num_thresh=50000):
 
         dists1 = np.ones(nv1) * 1000000.
         dists2 = np.ones(nv2) * 1000000.
+        if return_index:
+            min_indices1 = np.ones(nv1) * -1
+            min_indices2 = np.ones(nv2) * -1
         for i,v1 in enumerate(vq1):
             idx00 = i * num_thresh
             idx01 = i * num_thresh + len(v1)
@@ -71,11 +74,27 @@ def memory_safe_min_distances(voxels1, voxels2, num_thresh=50000):
                 idx11 = j * num_thresh + len(v2)
 
                 d = distance_matrix(v1, v2)
-                dists1[idx00:idx01] = np.minimum(d.min(axis=1), dists1[idx00:idx01])
-                dists2[idx10:idx11] = np.minimum(d.min(axis=0), dists2[idx10:idx11])
+                dmin1 = d.min(axis=1)
+                dmin0 = d.min(axis=0)
+                dists1[idx00:idx01] = np.minimum(dmin1, dists1[idx00:idx01])
+                dists2[idx10:idx11] = np.minimum(dmin0, dists2[idx10:idx11])
+                if return_index:
+                    dargmin1 = np.argmin(d, axis=1)
+                    dargmin0 = np.argmin(d, axis=0)
+                    mask1 = np.nonzero(dmin1 < dists1[idx00:idx01])
+                    min_indices1[idx00:idx01][mask1[0]] = dargmin1[mask1[0]] + idx00
+                    mask0 = np.nonzero(dmin0 < dists2[idx10:idx11])
+                    min_indices2[idx10:idx11][mask0[0]] = dargmin0[mask0[0]] + idx10
     else:
         pdist = distance_matrix(voxels1, voxels2)
         dists1 = pdist.min(axis=1)
         dists2 = pdist.min(axis=0)
-    return dists1, dists2
+        if return_index:
+            min_indices1 = pdist.argmin(axis=1)
+            min_indices2 = pdist.argmin(axis=0)
+
+    if return_index:
+        return dists1, dists2, min_indices1, min_indices2
+    else:
+        return dists1, dists2
 
