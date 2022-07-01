175dcfa5cd429f8314e04145e8a231d2e2160033
Zuohan Zhao <zzhmark@126.com>
2022-07-01 21:30:29 +0800

    done

--- file_io.py
+++ file_io.py
@@ -11,112 +11,36 @@
 #================================================================
 import SimpleITK as sitk
 import pickle
-import os
-import struct
-import numpy as np
-import sys
-import subprocess
-import platform
-
-
-def load_v3draw(path: str):
-    """
-    by Zuohan Zhao
-    from basic_c_fun/stackutils.cpp
-    2022/5/8
-    """
-    assert os.path.exists(path)
-    formatkey = "raw_image_stack_by_hpeng"
-    with open(path, "rb") as f:
-        filesize = os.path.getsize(path)
-        assert(filesize >= len(formatkey) + 2 + 4*4 + 1)
-        format = f.read(len(formatkey)).decode('utf-8')
-        assert(format == formatkey)
-        endianCodeData = f.read(1).decode('utf-8')
-        if endianCodeData == 'B':
-            endian = '>'
-        elif endianCodeData == 'L':
-            endian = '<'
-        else:
-            raise Exception('endian be big/little')
-        datatype = struct.unpack(endian+'h', f.read(2))[0]
-        if datatype == 1:
-            dt = 'u1'
-        elif datatype == 2:
-            dt = 'u2'
-        elif datatype == 4:
-            dt = 'f4'
-        else:
-            raise Exception('datatype be 1/2/4')
-        sz = struct.unpack(endian+'iiii', f.read(4*4))
-        tot = sz[0] * sz[1] * sz[2] * sz[3]
-        if tot * datatype + 4*4 + 2 + 1 + len(formatkey) != filesize:
-            f.seek(-4*2, 1)
-            tot = sz[0] * sz[1] * sz[2] * sz[3]
-            assert(tot * datatype + 4 * 4 + 2 + 1 + len(formatkey) == filesize)
-        img = np.frombuffer(f.read(tot), endian+dt)
-        return img.reshape(sz[-1:-5:-1])
-
-
-def save_v3draw(img: np.ndarray, path: str):
-    """
-    by Zuohan Zhao
-    from basic_c_fun/stackutils.cpp
-    2022/5/8
-    """
-    with open(path, 'wb') as f:
-        formatkey = "raw_image_stack_by_hpeng"
-        f.write(formatkey.encode())
-        if img.dtype.byteorder == '>':
-            endian = 'B'
-        elif img.dtype.byteorder == '<':
-            endian = 'L'
-        elif img.dtype == '|':
-            endian = 'B'
-        else:
-            if sys.byteorder == 'little':
-                endian = 'L'
-            else:
-                endian = 'B'
-        f.write(endian.encode())
-        if img.dtype == np.uint8:
-            datatype = 1
-        elif img.dtype == np.uint16:
-            datatype = 2
-        else:
-            datatype = 4
-        endian = '>' if endian == 'B' else '<'
-        f.write(struct.pack(endian+'h', datatype))
-        sz = list(img.shape)
-        sz.extend([0] * (4 - len(sz)))
-        sz.reverse()
-        f.write(struct.pack(endian+'iiii', *sz))
-        f.write(img.tobytes())
+# import subprocess
+# import platform
+from v3d.io import *
 
 
 def load_image(img_file: str, vaa3d="vaa3d", temp_dir=None):
-    if img_file.endswith("pbd"):
-        temp_file = img_file + "_temp.v3draw"
-        if platform.system() == "Windows":
-            subprocess.call(" ".join([vaa3d, "/x", "qc200k", "/f", "img_convert",
-                                      "/i", img_file, "/o", temp_file]))
-            img = load_v3draw(temp_file)
-            subprocess.call(" ".join(["rm", temp_file]))
-        else:
-            subprocess.call(" ".join([vaa3d, "-x", "qc200k", "-f", "img_convert",
-                                      "-i", img_file, "-o", temp_file]))
-            img = load_v3draw(temp_file)
-            subprocess.call(" ".join("del", temp_file))
-
+    # if img_file.endswith("pbd"):
+    #     temp_file = img_file + "_temp.v3draw"
+    #     if platform.system() == "Windows":
+    #         subprocess.call(" ".join([vaa3d, "/x", "qc200k", "/f", "img_convert",
+    #                                   "/i", img_file, "/o", temp_file]))
+    #         img = load_v3draw(temp_file)
+    #         subprocess.call(" ".join(["rm", temp_file]))
+    #     else:
+    #         subprocess.call(" ".join([vaa3d, "-x", "qc200k", "-f", "img_convert",
+    #                                   "-i", img_file, "-o", temp_file]))
+    #         img = load_v3draw(temp_file)
+    #         subprocess.call(" ".join("del", temp_file))
     if img_file.endswith(".v3draw"):
         return load_v3draw(img_file)
+    if img_file.endswith(".v3dpbd"):
+        return PBD().load_image(img_file)
     return sitk.GetArrayFromImage(sitk.ReadImage(img_file))
 
 
 def save_image(outfile: str, img: np.ndarray):
     if outfile.endswith(".v3draw"):
         save_v3draw(img, outfile)
-    sitk.WriteImage(sitk.GetImageFromArray(img), outfile)
+    else:
+        sitk.WriteImage(sitk.GetImageFromArray(img), outfile)
     return True
 
 
