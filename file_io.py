d37026411960ece5740b5604181c668cb05aa6b8
zzhmark@126.com <47688877+zzhmark@users.noreply.github.com>
2022-09-02 15:13:21 +0800

    Update file_io.py
    tif flip y

--- file_io.py
+++ file_io.py
@@ -11,20 +11,26 @@
 #================================================================
 import SimpleITK as sitk
 import pickle
+import numpy as np
 from v3d.io import *
 
 
 def load_image(img_file: str, vaa3d="vaa3d", temp_dir=None):
-    if img_file.endswith(".v3draw"):
+    if img_file.lower().endswith(".v3draw"):
         return load_v3draw(img_file)
-    if img_file.endswith(".v3dpbd"):
+    if img_file.lower().endswith(".v3dpbd"):
         return PBD().load_image(img_file)
-    return sitk.GetArrayFromImage(sitk.ReadImage(img_file))
+    img = sitk.GetArrayFromImage(sitk.ReadImage(img_file))
+    if img_file.lower().endswith(".tif") or img_file.lower().endswith(".tiff"):
+        img = np.flip(img, axis=-2)
+    return img
 
 
 def save_image(outfile: str, img: np.ndarray):
-    if outfile.endswith(".v3draw"):
+    if outfile.lower().endswith(".v3draw"):
         save_v3draw(img, outfile)
+    elif outfile.lower().endswith(".tif") or outfile.lower().endswith(".tiff"):
+        sitk.WriteImage(sitk.GetImageFromArray(np.flip(img, axis=-2)), outfile)
     else:
         sitk.WriteImage(sitk.GetImageFromArray(img), outfile)
     return True
