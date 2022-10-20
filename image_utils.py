93688afca97994a52e2a9c5e1cfe8a9a1e714124
crazylyf <crazylyf@163.com>
2022-10-20 21:03:30 +0800

    Max intensity projection of image support

--- image_utils.py
+++ image_utils.py
@@ -12,6 +12,18 @@
 
 from image_io import load_image, save_image
 
+
+def get_mip_image(img3d, axis=0, mode='MAX'):
+    if mode == 'MAX':
+        img2d = img3d.max(axis=axis)
+    elif mode = 'MIN':
+        img2d = img3d.min(axis=axis)
+    else:
+        raise ValueError
+
+    return img2d
+
+
 class AbastractCropImage:
     def __init__(self):
         pass
