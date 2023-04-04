00d0e41827e0439677bc34f17483cb7afe5fd793
crazylyf <crazylyf@163.com>
2023-04-04 16:38:30 +0800

    update metrics estimation

--- neuron_quality/metrics.py
+++ neuron_quality/metrics.py
@@ -16,8 +16,10 @@ from swc_handler import tree_to_voxels, parse_swc
 from math_utils import min_distances_between_two_sets
 
 class DistanceEvaluation(object):
-    def __init__(self, dsa_thr=2., esa_thr=2.):
+    def __init__(self, dsa_thr=2., resample1=True, resample2=True):
         self.dsa_thr = dsa_thr
+        self.resample1 = resample1
+        self.resample2 = resample2
 
     def calc_dist(self, voxels1, voxels2):
         ds = {
@@ -42,15 +44,29 @@ class DistanceEvaluation(object):
                 dists1_ = dists1
                 dists2_ = dists2
             ds[key] = dists1_.mean(), dists2_.mean(), (dists1_.sum() + dists2_.sum()) / (len(dists1) + len(dists2))
+        ds = np.array(list(ds.values()))
         return ds
 
     def run(self, reconfile, gsfile):
-        tree1 = parse_swc(reconfile)
-        tree2 = parse_swc(gsfile)
-        print(f'#nodes for recon and gs: {len(tree1)}, {len(tree2)}')
+        if type(reconfile) is str:
+            tree1 = parse_swc(reconfile)
+        else:
+            tree1 = reconfile
+        if type(gsfile) is str:
+            tree2 = parse_swc(gsfile)
+        else:
+            tree2 = gsfile
+        #print(f'#nodes for recon and gs: {len(tree1)}, {len(tree2)}')
 
-        voxels1 = tree_to_voxels(tree1, crop_box=(10000,10000,10000))
-        voxels2 = tree_to_voxels(tree2, crop_box=(10000,10000,10000))
+        if self.resample1:
+            voxels1 = tree_to_voxels(tree1, crop_box=(10000,10000,10000))
+        else:
+            voxels1 = np.array([node[2:5] for node in tree1])
+        if self.resample2:
+            voxels2 = tree_to_voxels(tree2, crop_box=(10000,10000,10000))
+        else:
+            voxels2 = np.array([node[2:5] for node in tree2])
+
         if len(voxels1) == 0 or len(voxels2) == 0:
             print(len(voxels1), len(voxels2))
             return False
