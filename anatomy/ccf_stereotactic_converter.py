28683bc7149a6c8eb3293d683133c54d639b0a5d
crazylyf <crazylyf@163.com>
2024-03-12 09:25:25 +0800

    minor fix

--- anatomy/ccf_stereotactic_converter.py
+++ anatomy/ccf_stereotactic_converter.py
@@ -11,6 +11,10 @@ from skimage.transform import rescale
 from file_io import load_image, save_image
 from anatomy.anatomy_config import MASK_CCF25_FILE
 
+__RX_CCF25__, __RY_CCF25__, __RZ_CCF25__ = 216, 18, 228
+__ROTATE_Z__ = -5.
+__SCALE_Y__ = 0.9434
+
 def matrix_from_axis_angle(a):
     """ Compute rotation matrix from axis-angle.
     This is called exponential map or Rodrigues' formula.
@@ -66,17 +70,23 @@ def get_center(img, w, h, d):
     """
     return img.TransformIndexToPhysicalPoint((w,h,d))
 
-def ccf2stereotactic_mask_res25(mask_file, stereo_file):
+def ccf2stereotactic_mask_res25(mask_file, stereo_file=None):
     """
     The majority of this implementation follows the instruction of the resources:
     1. https://community.brain-map.org/t/how-to-transform-ccf-x-y-z-coordinates-into-stereotactic-coordinates/1858
     2. https://stackoverflow.com/questions/56171643/simpleitk-rotation-of-volumetric-data-e-g-mri
     """
-    rx, ry, rz = 216, 18, 228
-    theta_z = -5.
-    sx = 0.9434
+    rx, ry, rz = __RX_CCF25__, __RY_CCF25__, __RZ_CCF25__
+    theta_z = __ROTATE_Z__
+    sx = __SCALE_Y__
 
-    image = sitk.ReadImage(mask_file)
+    if type(mask_file) is str:
+        image = sitk.ReadImage(mask_file)
+    elif type(mask_file) is np.ndarray:
+        image = sitk.GetImageFromArray(mask_file)
+    else:
+        raise ValueError('Incorrect input type for argument: `mask_file`')
+
     theta_z = np.deg2rad(theta_z)
     euler_transform = sitk.Euler3DTransform()
     image_center = get_center(image, rx, ry, rz)    #228,18,216 (z,y,x)
@@ -91,7 +101,8 @@ def ccf2stereotactic_mask_res25(mask_file, stereo_file):
     #resampled_image = resample(resampled_image, scale_transform)
     resampled_image = rescale(sitk.GetArrayFromImage(resampled_image), 
                 (1,sx,1), order=0, mode='constant', preserve_range=True)
-    save_image(stereo_file, resampled_image, useCompression=True)
+    if stereo_file:
+        save_image(stereo_file, resampled_image, useCompression=True)
 
     return resampled_image
 
